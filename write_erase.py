c64163fee0c7f4faba3e8dd34f236cfd174c645b
EC2 Default User <ec2-user@ip-172-31-37-1.eu-north-1.compute.internal>
2022-04-30 18:32:41 +0200

    Added python script to reset SONiC device to default.

---
+++ write_erase.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import paramiko
+import time
+import getpass
+import os
+#from host_seedfile import network_devices
+#from host_config_file import host_conf
+
+inventoryfile = 'hosts'
+hoststr = 'ansible_host='
+UN = 'admin'
+PW = 'admin123'
+commandlist = [ "sonic-cli", "write erase", "y", "reboot" ]
+ 
+#UN = input("Username : ")
+#PW = getpass.getpass("Password : ")
+
+
+def create_host_list (inventoryfile):
+    
+    """
+    This function searches for ansible hosts in the ansible invnentory file
+    This file defaults to 'hosts'
+    The Ip addresses of the hosts are returned in a list
+    If no hosts found, quit and do nothing.
+    """
+
+    hostlist = []
+
+    with open(inventoryfile) as f:
+        datafile = f.readlines()
+
+    for line in datafile:
+        index = line.find(hoststr)
+        if index != -1: #Found ansible host line
+            sliced = line[index+len(hoststr):].split() #Strip all leading chars till host ip-address and breakup on spaces
+            hostlist.append(sliced[0]) #Add IP address to list
+
+    hosts = len(hostlist)    
+    print()
+
+    if hosts == 0:
+        print('No hosts found in inventory file. No action taken.\n')
+        quit()
+    else:
+        print('Found ' + str(hosts) + ' hosts in ansible inventory. Will execute on following list:')
+        print(hostlist,'\n')
+        time.sleep(1)
+
+    return hostlist
+
+
+def execute_commands (hosts, commands):
+    
+    for ip in hosts:
+        print('Accessing ' + ip + ':\n')
+         
+        twrssh = paramiko.SSHClient()
+        twrssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        twrssh.connect(ip, port=22, username=UN, password=PW)
+        remote = twrssh.invoke_shell()
+        
+        for cmd in commands:
+            print('Send command: ' + cmd)
+            #remote.send(cmd + '\r')
+            time.sleep(1)
+            buf = remote.recv(65000)
+            print (buf)
+            f = open('sshlogfile0001.txt', 'ab')
+            f.write(buf)
+            f.close()
+
+        print('\n- OK -- Host should reboot with default config, but retains Mgt IP ' + ip)
+        print('----------------------------------------------------------------------------------\n')
+
+        twrssh.close()
+
+
+
+## Start main program
+## ==================
+iplist = create_host_list(inventoryfile) #Create list with ip addresses of all hosts
+execute_commands (iplist, commandlist)   #Login to hosts and execute commands
+
+print('All Done.')
