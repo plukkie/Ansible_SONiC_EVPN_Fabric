0db6903a22a7bcc29b05b7af8088dd1ea396a4dd
Peter Plak <peter_plak@dell.com>
2022-05-03 11:49:04 +0000

    optimized functionality

--- write_erase.py
+++ write_erase.py
@@ -4,6 +4,7 @@ import paramiko
 import time
 import getpass
 import os
+import sys
 #from host_seedfile import network_devices
 #from host_config_file import host_conf
 
@@ -17,7 +18,7 @@ commandlist = [ "sonic-cli", "write erase", "y", "reboot" ]
 #PW = getpass.getpass("Password : ")
 
 
-def create_host_list (inventoryfile):
+def create_host_list (inventoryfile, singlehost=None):
     
     """
     This function searches for ansible hosts in the ansible invnentory file
@@ -31,17 +32,26 @@ def create_host_list (inventoryfile):
     with open(inventoryfile) as f:
         datafile = f.readlines()
 
-    for line in datafile:
+    for line in datafile: #Search line for line in inventoryfile
         index = line.find(hoststr)
         if index != -1: #Found ansible host line
-            sliced = line[index+len(hoststr):].split() #Strip all leading chars till host ip-address and breakup on spaces
-            hostlist.append(sliced[0]) #Add IP address to list
+            if (singlehost == False): 
+                sliced = line[index+len(hoststr):].split() #Strip all leading chars till host ip-address and breakup on spaces
+                hostlist.append(sliced[0]) #Add IP address to list
+            else: #There a single host arg given
+                if (singlehost in line): #Found host or ip in ansible inventory file
+                    sliced = line[index+len(hoststr):].split() #Strip all leading chars till host ip-address and breakup on spaces
+                    hostlist.append(sliced[0]) #Add IP address to list
+                    break
 
     hosts = len(hostlist)    
     print()
 
-    if hosts == 0:
+    if (hosts == 0 and singlehost == False):
         print('No hosts found in inventory file. No action taken.\n')
+        quit()
+    elif (hosts == 0 and singlehost != False):
+        print(singlehost + ' was not found in inventory file. Typo?\n')
         quit()
     else:
         print('Found ' + str(hosts) + ' hosts in ansible inventory. Will execute on following list:')
@@ -98,18 +108,32 @@ def yes_or_no(question):
             pass
 
 
+def get_cli_argument():
+
+    arguments = len(sys.argv)
+    if (arguments == 1): #No cli option given
+        return False
+    else:
+        arglist = str(sys.argv[1]) #Fetch cli argument (should be a ansible host ip or name)
+        return arglist
+
+
+
 ## ==================
 ## Start main program
 ## ==================
 
 print()
+
+singlehost = get_cli_argument() #Check if started with a singlehost request
+iplist = create_host_list(inventoryfile, singlehost)  #Create list with ip addresses of all hosts
+
 result = yes_or_no ('Are you sure you want to delete startup-configs and reboot hosts from ansible iventory?')
 if (result == False):
     print()
     quit()
 
-iplist = create_host_list(inventoryfile) #Create list with ip addresses of all hosts
 execute_commands (iplist, commandlist)   #Login to hosts and execute commands
 
 print('All Done.\n')
-
+ 
